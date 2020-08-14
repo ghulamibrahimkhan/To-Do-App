4725b653d2df86fe90b3f7f6bc76f6365ef34fac
ghulamibrahimkhan <gulamibrahimkhan@gmail.com>
2020-08-15 01:54:08 +0500

    Todo structure completed

--- public/app.js
+++ public/app.js
@@ -2,24 +2,34 @@
 var list = document.getElementById("list")
 
 // To do function
-function addTodo(){
+function addTodo() {
     // getting user input
-   var toDoItem = document.getElementById("to-do-item")
+    var toDoItem = document.getElementById("to-do-item")
 
     // creating <li> to put in <ul>
-   var li = document.createElement("li")
+    var li = document.createElement("li")
 
-    // user input value to insett in li
-   var liText = document.createTextNode(toDoItem.value)
+    // user input value to insert in li
+    var liText = document.createTextNode(toDoItem.value)
 
     // inserting user input in li
-   li.appendChild(liText)
- 
+    li.appendChild(liText)
+
     // inserting <li> in to <ul>
     list.appendChild(li)
 
 
-//    ****Delete Button****
+    // ******Edit Buton******
+
+
+    var editButton = document.createElement("button")
+    var editText = document.createTextNode("E d i t")
+    editButton.appendChild(editText)
+    li.appendChild(editButton)
+    editButton.setAttribute("onclick", "editItem(this)")
+
+
+    //    ****Delete Button****
 
     // creating delete button
     var deleteButton = document.createElement("button")
@@ -30,14 +40,32 @@ function addTodo(){
     // inserting button in <li>
     li.appendChild(deleteButton)
     // setting attribute
-    deleteButton.setAttribute("class","button")
+    deleteButton.setAttribute("class", "button")
     //* passing whole button to the function "deleteTask" 
-    deleteButton.setAttribute("onclick","deleteTask(this)")
+    deleteButton.setAttribute("onclick", "deleteTask(this)")
 }
 
 
 // *******Delete task function*********
-    // taking data on click from deleteButton 
-function deleteTask(pass){
+// taking data on click from deleteButton 
+function deleteTask(pass) {
     pass.parentNode.remove()
+}
+
+
+
+// **********Edit Task Function********
+function editItem(edit) {
+
+    var val = edit.parentNode.firstChild.nodeValue
+
+    var editValue = prompt("Enter your task",val)
+
+    edit.parentNode.firstChild.nodeValue = editValue;
+}
+
+
+// *****Delete All button
+function deleteAll() {
+    list.innerHTML = ""
 }
